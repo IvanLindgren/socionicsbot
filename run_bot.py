1017f6900369d2a20b72ceb15de0ba462d223828
Denis Kornilov <denis.kornilov12@yandex.ru>
2024-11-12 05:42:27 +0300

    design

--- run_bot.py
+++ run_bot.py
@@ -13,13 +13,6 @@ import os
 
 import tensorflow as tf
 
-gpus = tf.config.experimental.list_physical_devices('CPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
 
 
 
